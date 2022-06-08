760e7460e2fdb4b5d8839a117a3ee4aa74a15b48
Ali Basiri <57348384+DragonTamer117@users.noreply.github.com>
2022-06-08 10:27:23 +0200

    HTTPS security
    Made 2 methods that made an endpoint for the HTTPS.

---
+++ https.js
@@ -0,0 +1,27 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    hostname: 'https://localhost:4200',
+    port: 4200,
+    path: '/',
+    method: 'GET';
+};
+
+https.createServer(options, (req, res) => {
+    res.writeHead(200);
+    res.end('Succesfully connected\n');
+}).listen(4200);
+
+const httpsGet = https.get('https://localhost:4200', (res) => {
+    console.log('Statuscode: ', res.statusCode);
+    console.log('Headers: ', res.headers);
+
+    res.on('Data: ', (d) => {
+        process.stdout.write(d);
+    });
+
+}).on('Error: ', (e) => {
+    console.error(e);
+});
+
