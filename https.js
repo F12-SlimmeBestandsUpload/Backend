2d1180c92892e3410b37ae8838d6ae65e0aa34dc
FlodekNL <50317497+FlodekNL@users.noreply.github.com>
2022-06-21 17:44:28 +0200

    HTTPS Refactor
    - Refactored HTTPS

--- https.js
+++ https.js
@@ -1,36 +1,31 @@
-const https = require('https');
-const fs = require('fs');
-const key = './cert/key.pem';
-const cert = './cert/cert.pem';
-const privkey = './cert/privkey.pem';
+module.exports = (app, port) => {
+    const https = require('https');
+    const fs = require('fs');
+    const key = process.env.PRIVATE_KEY_PATH;
+    const cert = process.env.CERTIFICATE_PATH;
 
-//TODO: Remove this disgusting hack.
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-const options = {
-    passphrase: "1234",
-    hostname: 'http://localhost:4200',
-    port: 4200,
-    path: '/',
-    method: 'GET',
-    key: fs.readFileSync(key, 'utf8'),
-    cert: fs.readFileSync(cert, 'utf8')
-};
 
-https.createServer(options, (req, res) => {
-    res.writeHead(200);
-    res.end('Succesfully connected\n');
-}).listen(4200);
+    if (process.env.DEV_MODE == "true") {
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    }
 
-const httpsGet = https.get('https://localhost:4200', (res) => {
-    console.log('Statuscode: ', res.statusCode);
-    console.log('Headers: ', res.headers);
 
-    res.on('Data: ', (d) => {
-        process.stdout.write(d);
-    });
 
-}).on('Error: ', (e) => {
-    console.error(e);
-});
+    const options = {
+        hostname: '0.0.0.0',
+        port: port,
+        key: fs.readFileSync(key, 'utf8'),
+        cert: fs.readFileSync(cert, 'utf8')
+    };
 
+
+
+    if (process.env.PRIVATE_KEY_PASSPHRASE != 'null') {
+        options.passphrase = process.env.PRIVATE_KEY_PASSPHRASE;
+    }
+
+
+
+    return https.createServer(options, app);
+}
