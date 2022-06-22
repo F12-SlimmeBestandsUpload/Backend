97d6c0811999d9245d15031e9ba3b95703f622df
c9d5e1 <47698642+c9d5e1@users.noreply.github.com>
2022-06-22 10:30:25 +0200

    fix

--- StatusResponseService.js
+++ StatusResponseService.js
@@ -1,39 +1,42 @@
-module.exports = StatusResponseService
+module.exports = () => {
 
-class StatusResponseService{
+    class StatusResponseService{
 
-    constructor() {
+        constructor() {
+        }
+
+        response(statusCode, value, message) {
+            return JSON.stringify({value, message});
+        }
+
+        succesResponse(message) {
+            this.response(200, true, message);
+        }
+
+        badRequestResponse() {
+            this.response(400, false, "400 Bad Request")
+        }
+
+        unauthorizedResponse() {
+            this.response(401, false, "401 Unauthorized")
+        }
+
+        ForbiddenResponse() {
+            this.response(403, false, "403 Forbidden")
+        }
+
+        notFoundErrorResponse() {
+            this.response(404, false, "404 Not Found")
+        }
+
+        notAcceptableResponse() {
+            this.response(406, false, "406 Bad Content Type/Parameters are missing")
+        }
+
+        internalServerErrorResponse() {
+            this.response(500, false, "500 Internal Server Error")
+        }
     }
 
-    response(statusCode, value, message) {
-        return JSON.stringify({value, message});
-    }
-
-    succesResponse(message) {
-        this.response(200, true, message);
-    }
-
-    badRequestResponse() {
-        this.response(400, false, "400 Bad Request")
-    }
-
-    unauthorizedResponse() {
-        this.response(401, false, "401 Unauthorized")
-    }
-
-    ForbiddenResponse() {
-        this.response(403, false, "403 Forbidden")
-    }
-
-    notFoundErrorResponse() {
-        this.response(404, false, "404 Not Found")
-    }
-
-    notAcceptableResponse() {
-        this.response(406, false, "406 Bad Content Type/Parameters are missing")
-    }
-
-    internalServerErrorResponse() {
-        this.response(500, false, "500 Internal Server Error")
-    }
+    return new StatusResponseService();
 }
